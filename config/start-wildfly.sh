8fa9d710b2e9d3d94d4b3e615e331ce579e66bde
Salim Vanak <s.vanak@mediamagic.ltd.uk>
2017-10-04 16:21:50 +0100

    Updated gradle and modified the grable build for docker The docker build now builds a Postgresql docker image with the database ddl scrips in the init director ready of the docker image to build the database when it first starts up.

--- config/start-wildfly.sh
+++ config/start-wildfly.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # wait 5 seconds for the db services to start.
 
-$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB
+if [ -n "$DEBUG" ]; then
+	$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB --debug $DEBUG;
+else
+	$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB;
+fi
