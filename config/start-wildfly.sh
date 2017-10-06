7db0732692b645a81a9113c4d8f34017d7140269
Salim Vanak <s.vanak@mediamagic.ltd.uk>
2017-10-06 14:13:07 +0100

    Added java.headless var to stop errors from access to java.awt by Jasper Reports

--- config/start-wildfly.sh
+++ config/start-wildfly.sh
@@ -2,7 +2,7 @@
 # wait 5 seconds for the db services to start.
 
 if [ -n "$DEBUG" ]; then
-	$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB --debug $DEBUG;
+	$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB -Djava.awt.headless=true -Djava.io.tmpdir=/tmp-java/ --debug $DEBUG;
 else
-	$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB;
+	$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -Dpostgres.uri=$POSTGRES_URI -Dpostgres.dbname=$POSTGRES_DB -Djava.awt.headless=true -Djava.io.tmpdir=/tmp-java/;
 fi
