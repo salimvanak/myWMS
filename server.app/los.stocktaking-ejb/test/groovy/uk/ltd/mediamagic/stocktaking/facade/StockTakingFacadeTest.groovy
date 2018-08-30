2535356fcbaf66822f77db5222b9c54121b767a2
salimvanak <mail@salim.me.uk>
2018-08-30 13:47:18 +0100

    Added some tests for stock taking

--- server.app/los.stocktaking-ejb/test/groovy/uk/ltd/mediamagic/stocktaking/facade/StockTakingFacadeTest.groovy
+++ server.app/los.stocktaking-ejb/test/groovy/uk/ltd/mediamagic/stocktaking/facade/StockTakingFacadeTest.groovy
@@ -4,6 +4,8 @@ import static org.junit.Assert.*
 
 import java.util.Date
 
+import org.junit.BeforeClass
+
 import de.linogistix.los.stocktaking.facade.LOSStocktakingFacade
 import spock.lang.Specification
 import uk.ltd.mediamagic.los.inventory.test.ItemDataSpec
@@ -13,20 +15,33 @@ import uk.ltd.mediamagic.los.inventory.test.UnitLoadSpec
 
 class StockTakingFacadeTest extends Specification  implements StorageLocationSpec, UnitLoadSpec, StockUnitSpec, ItemDataSpec {
 
-	static LOSStocktakingFacade stocktakeFacade;
-	
-	def "Create a stock taking order"() {
+	static LOSStocktakingFacade stocktakeFacade
+
+  @BeforeClass
+  def setupBeans() {
+    stocktakeFacade = getBean(LOSStocktakingFacade.class)
+  }
+
+	def "Stock take a location to empty"() {
 		given:
-			def startLoc = createStorageLocation("LOCSTART")
+		  def startLoc = createStorageLocation("LOCSTART")
 			def endLoc = createStorageLocation("LOCEND")
 			def ul = createUnitLoad("STORE", startLoc)
 			def su = createStockUnit(ul, createItemData(), 5);
 			
 		when: "Store the unit load on destination"
-			orderCount = stocktakeFacade.generateOrders(true, null, null, null, null, sl.getId(), null, null, null, null, true, true)
+			def orderCount = stocktakeFacade.generateOrders(true, null, null, null, null, startLoc.getId(), null, null, null, null, true, true)
 		then:
 			orderCount == 1
-	}
+      ulQuery.countUnitLoadsByStorageLocation(startLoc) == 1
+
+    when: "the location is empty"
+      stocktakeFacade.processLocationEmpty(startLoc)
+    then: "Then there are no more locations"
+      stocktakeFacade.getNextLocation(getClient(), startLoc.getName()) == null
+      ulQuery.countUnitLoadsByStorageLocation(startLoc) == 0
+  }
+
 
 
 }
