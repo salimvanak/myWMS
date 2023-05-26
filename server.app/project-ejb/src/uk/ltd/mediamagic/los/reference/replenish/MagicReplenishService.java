58bb05968414ea01ed9918da7b35fcd90d8be821
salimvanak <mail@salim.me.uk>
2023-05-26 15:00:31 +0100

    added documentation to MagicReplenishService

--- server.app/project-ejb/src/uk/ltd/mediamagic/los/reference/replenish/MagicReplenishService.java
+++ server.app/project-ejb/src/uk/ltd/mediamagic/los/reference/replenish/MagicReplenishService.java
@@ -22,18 +22,18 @@ public interface MagicReplenishService {
 	/**
 	 * Replenish the item data.
 	 * If more than one fixed location exists a location is selected.  
-	 * We do not contract which location is selected.
+	 * No assertion is made as to which location is selected if there are multiple fixed
+	 * locations to choose from.
 	 * 
-	 * If this method returns null then it is the case that non of the fixed
-	 * locations require replenish.  i.e. if any of the fixed locations needs
-	 * replenishing.
+	 * This method will return null if none of the fixed location require replenishment.
 	 * 
-	 * This method may behave differently if the amountReqired is provided.
-	 * with an amount may trigger a replenish when a null value might not.
+	 * If the <code>amountRequired</code> is null, the replenish is only requested if the 
+	 * is required.
 	 * 
-	 * If the <code>amountRequired</code> is will force the method to insure
-	 * that the amount is available on the picking location.  If the amount required
-	 * is more than the capacity of the location the location will be fill to its maximum.
+	 * If the <code>amountRequired</code> is not null, the method to insure
+	 * that the <code>amountRequired</code> is available on the picking locations.  
+	 * If the <code>amountRequired</code> required is more than the capacity of the location 
+	 * the location will be filled to its maximum.
 	 * 
 	 * @param itemData the item data.
 	 * @param amountReqired the amount required at one of the pick locations.  If null the system will 
@@ -45,12 +45,12 @@ public interface MagicReplenishService {
 
 	/**
 	 * Replenish the storage location.
-	 * If the location provided is not a fixed location then this method can ignore the request.
+	 * If the location provided is not a fixed location then this method will ignore the request.
 	 * 
-	 * This method may behave differently if the amountReqired is provided.
-	 * with an amount may trigger a replenish where a null value may not.
+	 * If the <code>amountRequired</code> is null, the replenish is only requested if the 
+	 * is required.
 	 * 
-	 * If the <code>amountRequired</code> is will force the method to insure
+	 * If the <code>amountRequired</code> is not null, the method to insure
 	 * that the amount is available on the picking location.  If the amount required
 	 * is more than the capacity of the location the location will be filled to its maximum.
 	 * 
